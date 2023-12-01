9e455f91210d8fb9315cf4223bbe6406c0548b05
carleyfaulkner <145037495+carleyfaulkner@users.noreply.github.com>
2023-12-01 16:50:15 -0600

    Add files via upload

---
+++ project-three/change.js
@@ -0,0 +1,28 @@
+function change01() {
+   var largepic= document.getElementById("large");
+   largepic.src="../images/page-of-spell-book.png";
+
+}
+
+function change02() {
+    var largepic= document.getElementById("large");
+    largepic.src="../images/bundle-scrolls.png";
+ 
+ }
+ function change03() {
+    var largepic= document.getElementById("large");
+    largepic.src="../images/group-potion.png";
+ 
+ }
+ function change04() {
+    var largepic= document.getElementById("large");
+    largepic.src="../images/old-image-spellbook.png";
+ 
+ }
+ function change05() {
+    var largepic= document.getElementById("large");
+    largepic.src="../images/ingredients-potions.png";
+ 
+ }
+
+ 
